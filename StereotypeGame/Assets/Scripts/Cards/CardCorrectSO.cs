857eb0e8407180d243291aef6f1f6a28bc2940c4
Ejm3n <yashaydakov@mail.ru>
2022-02-18 14:33:01 +0200

    добавил новые факты

---
+++ StereotypeGame/Assets/Scripts/Cards/CardCorrectSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New correct list", menuName = "CardCorrect")]
+public class CardCorrectSO : ScriptableObject
+{
+    public string[] Content; 
+}
--- StereotypeGame/Assets/Scripts/ObjectPooling/Spawner.cs
+++ StereotypeGame/Assets/Scripts/ObjectPooling/Spawner.cs
@@ -21,8 +21,12 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int cardCount;
     [SerializeField] private bool autoExpand = true;
     [SerializeField] private CardController cardPrefab;
+    [Header ("Старые карточные скриптаблы")]
     [SerializeField] private CardSO easySentences;
     [SerializeField] private CardSO hardSentences;
+    [Header("Новые правильные карточные скриптаблы")]
+    [SerializeField] private CardCorrectSO easyCorrect;
+    [SerializeField] private CardCorrectSO hardCorrect;
     [SerializeField] private float timeToCreateCard;
     [SerializeField] private Vector2[] positions;
     [SerializeField] private float minLeftSpawn;
@@ -41,13 +45,13 @@ public class Spawner : MonoBehaviour
     {
         pool = new ObjectPool<CardController>(cardPrefab, cardCount, transform);
         pool.AutoExpand = autoExpand;
-        easyCardContent = RandomizeCardContent(easyCardContent, easySentences);
-        hardCardContent = RandomizeCardContent(hardCardContent, hardSentences);
+        easyCardContent = RandomizeCardContent(easyCardContent, easySentences,easyCorrect);
+        hardCardContent = RandomizeCardContent(hardCardContent, hardSentences,hardCorrect);
         
         StartCoroutine(TimerToCreation());
     }
 
-    private List<ContentStorage> RandomizeCardContent(List<ContentStorage> list, CardSO contentIn)
+    private List<ContentStorage> RandomizeCardContent(List<ContentStorage> list, CardSO contentIn,CardCorrectSO correctContent)
     {
        
         while(true)
@@ -78,6 +82,17 @@ public class Spawner : MonoBehaviour
             int randomIndex = Random.Range(i, list.Count);
             list[i] = list[randomIndex];
             list[randomIndex] = temp;
+        }
+        List<string> correctAnswers = correctContent.Content.OrderBy(x => UnityEngine.Random.Range(0, correctContent.Content.Length)).ToList();
+        int correctCurrentNum = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            if(!list[i].IsStereotype)
+            {
+                list[i].Content.Fact = correctAnswers[correctCurrentNum];
+                correctCurrentNum++;
+            }
+                
         }
         return list;
     }
@@ -129,7 +144,6 @@ public class Spawner : MonoBehaviour
         else
             card.transform.position = new Vector2( Random.Range(minRightSpawn, maxRightSpawn),ySpawn);
 
-        //card.transform.position = positions[currentSpawnPoint];
         card.SpriteHolder.rotation = Quaternion.Euler(0, 0, Random.Range(-rotationMax, rotationMax));
         if (isEasy)
             card.CardContent = easyCardContent[currentCardNum].Content;
